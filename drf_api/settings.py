c6948886f3415c3b771d169002474d17107608af
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-11-30 12:04:23 +0000

    Added post list view.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -36,10 +36,11 @@ SECRET_KEY = 'django-insecure-%=f1v$zj#fj596zha2(888+r3#79^8r&u*)hck(o9#&wu1^s_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-raypt808-djangorestfram-qes15p19prq.ws-eu116.gitpod.io']
+ALLOWED_HOSTS = ['8000-raypt808-djangorestfram-q27b7m0yvs7.ws-eu117.gitpod.io']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-raypt808-djangorestfram-qes15p19prq.ws-eu116.gitpod.io',
+    'https://8000-raypt808-djangorestfram-q27b7m0yvs7.ws-eu117.gitpod.io',
 ]
 
 
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'profiles',
+    'posts', 
 ]
 
 MIDDLEWARE = [
