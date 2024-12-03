f855074760be51c3e3251bbeea2a34e8072f3bb3
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-12-03 20:22:37 +0000

    Prepared for deployment.

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 import os
+
 os.environ['CLOUDINARY_URL'] = 'cloudinary://182864721262978:1WCkX0W4i51y7o-HEyEar4J4qmY@dqavku7fd'  
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:CaJEe8qYldk6@ep-bold-voice-a237kjaf.eu-central-1.aws.neon.tech/blob_youth_mouth_254327"
+os.environ.setdefault("SECRET_KEY", "RandomSecretSomethingNothing")
+
