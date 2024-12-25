1b00086a3d46cd7e402bd9c10671c555a7d0813d
RayPT808 <qiauxirt@students.codeinstitute.net>
2024-12-25 15:10:58 +0000

    Modified settings.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -142,9 +142,14 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
 #          os.environ.get('CLIENT_ORIGIN')
 #  ]
 #else:
-#    CORS_ALLOWED_ORIGIN_REGEXES = [
-#       r"^https:\/\/.*\.codeinstitute-ide\.net$",
-#  ]
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+       r"^https:\/\/.*\.codeinstitute-ide\.net$",
+  ]
+
+    CORS_ALLOWED_ORIGINS = [
+    "https://3000-raypt808-moments-75v0hs57mct.ws-eu117.gitpod.io/signup",  # Replace with your frontend's URL
+]
+
 
  
 CORS_ALLOW_CREDENTIALS = True    
